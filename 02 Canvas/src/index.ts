db5d1760a4f2dfd27bfb6c06c4d0936b7c1479db
Michal Radomski <michalra@op.pl>
2023-08-20 23:41:34 +0200

    Canvas course - commit 03

--- 02 Canvas/src/index.ts
+++ 02 Canvas/src/index.ts
@@ -1,1 +1,15 @@
 // console.log("test");
+
+const myCanvas = document.querySelector("#myCanvas") as HTMLCanvasElement;
+console.log("myCanvas:", myCanvas);
+
+function getMousePosition(canvas: HTMLCanvasElement, event: MouseEvent): void {
+  let rect = canvas.getBoundingClientRect();
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+  console.log("Coordinate x: " + x, "Coordinate y: " + y);
+}
+
+myCanvas.addEventListener("mousedown", function (event: MouseEvent) {
+  getMousePosition(myCanvas, event);
+});
