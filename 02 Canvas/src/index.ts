ecf4f7b6a90b2ce51b7acac86833901e875c8084
Michal Radomski <michalra@op.pl>
2023-08-21 02:02:03 +0200

    Canvas course - commit 10

--- 02 Canvas/src/index.ts
+++ 02 Canvas/src/index.ts
@@ -1,5 +1,6 @@
 // console.log("test");
 
+//^ Canvas 1
 const myCanvas = document.querySelector("#myCanvas") as HTMLCanvasElement;
 // console.log("myCanvas:", myCanvas);
 
@@ -48,6 +49,7 @@ if (myCanvas.getContext("2d")) {
   ctx.lineTo(400, 20);
   ctx.lineWidth = 3;
   ctx.stroke();
+  ctx.closePath();
 
   // Second path
   ctx.beginPath();
@@ -56,6 +58,7 @@ if (myCanvas.getContext("2d")) {
   ctx.lineTo(320, 120);
   ctx.lineWidth = 2;
   ctx.stroke();
+  ctx.closePath();
 
   // Third and fourth path
   ctx.beginPath();
@@ -67,6 +70,7 @@ if (myCanvas.getContext("2d")) {
   // ctx.fillStyle = "black";
   // ctx.fill();
   ctx.stroke();
+  ctx.closePath();
 
   //* Circle
   ctx.beginPath();
@@ -74,6 +78,7 @@ if (myCanvas.getContext("2d")) {
   ctx.lineWidth = 3;
   ctx.arc(250, 200, 50, 0, 2 * Math.PI);
   ctx.stroke();
+  ctx.closePath();
 
   //* Arc
   ctx.beginPath();
@@ -81,6 +86,7 @@ if (myCanvas.getContext("2d")) {
   ctx.lineWidth = 3;
   ctx.arc(250, 300, 50, 0, 1 * Math.PI);
   ctx.fill();
+  ctx.closePath();
 
   //* BezierCurve
   ctx.beginPath();
@@ -88,6 +94,7 @@ if (myCanvas.getContext("2d")) {
   ctx.moveTo(250, 400);
   ctx.bezierCurveTo(250, 500, 400, 500, 400, 120);
   ctx.stroke();
+  ctx.closePath();
 
   //* quadratic BezierCurve
   ctx.beginPath();
@@ -96,6 +103,7 @@ if (myCanvas.getContext("2d")) {
   ctx.moveTo(20, 520);
   ctx.quadraticCurveTo(20, 600, 200, 520);
   ctx.stroke();
+  ctx.closePath();
 
   //* BezierCurve in a for loop
   for (let i = 0; i < 10; i++) {
@@ -104,7 +112,47 @@ if (myCanvas.getContext("2d")) {
     ctx.strokeStyle = `rgba(${15 * i}, ${15 * i}, ${15 * i}, ${1 - 0.05 * i})`;
     ctx.bezierCurveTo(250 + 10 * i, 500 + 10 * i, 400 + 10 * i, 500 + 10 * i, 400 + 10 * i, 120 + 10 * i);
     ctx.stroke();
+    ctx.closePath();
   }
 } else {
   console.log("!myCanvas.getContext('2d')");
+}
+
+//^ Canvas 2
+const canvas = document.getElementById("myCan") as HTMLCanvasElement;
+if (canvas.getContext) {
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+  ctx.beginPath();
+  ctx.fill();
+  ctx.fillStyle = "yellow";
+  ctx.beginPath();
+  ctx.arc(320, 160, 150, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.moveTo(180, 170);
+  ctx.bezierCurveTo(180, 220, 330, 400, 450, 170);
+  ctx.lineTo(180, 170);
+  ctx.closePath();
+  ctx.fillStyle = "black";
+  ctx.fill();
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = "rgb(125,0,125)";
+  ctx.stroke();
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(250, 172, 50, 50);
+  ctx.fillRect(310, 172, 50, 50);
+
+  // Eyes
+  ctx.beginPath();
+  ctx.arc(255, 90, 30, 0, 2 * Math.PI);
+  ctx.fillStyle = "#333333";
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.arc(365, 90, 30, 0, 2 * Math.PI);
+  ctx.fillStyle = "#333333";
+  ctx.fill();
+  ctx.closePath();
 }
